05d62c781d04ad65018965c7d97175dcd6d38529
Neto test <anastasiya.parfinenko@gmail.com>
2023-07-10 14:26:27 +0600

    Array

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,11 +3,11 @@
 function solveEquation(a, b, c) {
 	if (a === 0) return false;
 
-	let arr = []
+	let arr = [];
 	let d = Math.pow(b, 2) - 4 * a * c
-	if (d < 0) return false;
+	if (d < 0) return arr;
 	if (d === 0) {
-		arr = -b / (2 * a)
+		arr =[ -b / (2 * a)]
 	} else if (d > 0) {
 		let tmp = []
 		tmp.push((-b + Math.sqrt(d)) / (2 * a));
